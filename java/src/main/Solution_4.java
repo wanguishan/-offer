b5a0590fe62e74cd34b81804e0607a13a74e915a
wanguishan <wanguishan@unicloud.com>
2019-12-03 17:11:04 +0800

    add Solution_4:二维数组中的查找

---
+++ java/src/main/Solution_4.java
@@ -0,0 +1,26 @@
+package main;
+
+// 二维数组中的查找
+//
+// 在一个二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
+// 请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+
+public class Solution_4 {
+    public boolean Find(int target, int[][] array) {
+        if (array == null || array.length == 0 || array[0].length == 0) {
+            return false;
+        }
+        int rows = array.length, cols = array[0].length;
+        int row = 0, col = cols - 1;
+        while (row < rows && col >= 0) {
+            if (target > array[row][col]) {
+                row++;
+            } else if (target < array[row][col]) {
+                col--;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
