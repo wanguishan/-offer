366081deedca5de6cf782b250d080131ed2b8ace
wanguishan <wanguishan@unicloud.com>
2019-12-05 13:17:03 +0800

    add TreeNode.java

---
+++ java/src/main/utils/TreeNode.java
@@ -0,0 +1,16 @@
+package main.utils;
+
+/**
+ * Definition for binary tree
+ *
+ * @author wanguishan
+ * @date 2019-12-5 13:12:10
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x) {
+        val = x;
+    }
+}
