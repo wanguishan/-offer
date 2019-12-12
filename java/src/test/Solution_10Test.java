8c841192080dd88d80d1b1581dc62971dbc1fb4b
wanguishan <wanguishan@unicloud.com>
2019-12-12 13:57:38 +0800

    add test-cherkers

--- java/src/test/Solution_10Test.java
+++ java/src/test/Solution_10Test.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.Solution_10;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,39 +16,31 @@ import java.util.Collection;
 public class Solution_10Test {
 
     private int input;
-    private int output;
 
-    public Solution_10Test(int input, int output) {
+    public Solution_10Test(int input) {
         this.input = input;
-        this.output = output;
     }
 
     @Parameters
     public static Collection date() {
-        Object[][] data = {
-                {0, 0},
-                {1, 1},
-                {2, 1},
-                {3, 2},
-                {4, 3},
-                {5, 5},
-                {6, 8},
-        };
+        Object[] data = {0, 1, 3, 6, 15, 39};
         return Arrays.asList(data);
     }
 
     @Test
     public void fibonacci_1() {
-        System.out.println("input: " + input + "; output: " + output);
         FibonacciChecker fibonacciChecker = new FibonacciChecker();
-        int actualOutput = fibonacciChecker.Fabonacci(input);
+        int output = fibonacciChecker.Fabonacci(input);
+        int actualOutput = Solution_10.Fibonacci_1(input);
+        System.out.println("input: " + input + ";\toutput: " + output + ";\tactualOutput: " + actualOutput);
         Assert.assertEquals(output, actualOutput);
     }
 
     @Test
     public void fibonacci_2() {
         FibonacciChecker fibonacciChecker = new FibonacciChecker();
-        int actualOutput = fibonacciChecker.Fabonacci(input);
+        int output = fibonacciChecker.Fabonacci(input);
+        int actualOutput = Solution_10.Fibonacci_2(input);
         Assert.assertEquals(output, actualOutput);
     }
 
