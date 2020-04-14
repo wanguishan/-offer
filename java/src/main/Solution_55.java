e4dd75d4984768289baf0e92c3633deba00005aa
wanguishan <wanguishan2010@163.com>
2020-04-14 22:35:05 +0800

    add Solution_69：二叉树的最近公共祖先

---
+++ java/src/main/Solution_55.java
@@ -0,0 +1,16 @@
+package main;
+
+import main.utils.TreeNode;
+
+/**
+ * 二叉树的深度
+ * 输入一棵二叉树的根节点，求该树的深度。从根节点到叶节点依次经过的节点（含根、叶节点）形成树的一条路径，最长路径的长度为树的深度
+ *
+ * @author Guishan Wan
+ * @date 2020/4/14 10:29 下午
+ */
+public class Solution_55 {
+    public int maxDepth(TreeNode root) {
+        
+    }
+}
--- java/src/main/Solution_69.java
+++ java/src/main/Solution_69.java
@@ -14,17 +14,17 @@ public class Solution_69 {
         if (root == null || root == p || root == q) return root;
 
         // 后序遍历，也是树的套路
-        TreeNode left = lowestCommonAncestor(root.left, p, q);
-        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        TreeNode l = lowestCommonAncestor(root.left, p, q);
+        TreeNode r = lowestCommonAncestor(root.right, p, q);
 
         // 写边界条件，也是后序遍历
-        if (left != null && right != null) return root;
+        if (l != null && r != null) return root;
 
-        return left == null ? right : left;
+        return l == null ? r : l;
 
-/*        if (left == null && right == null) return null;
-        if (left == null && right != null) return right;
-        if (left != null && right == null) return left;
+/*        if (l == null && r == null) return null;
+        if (l == null && r != null) return r;
+        if (l != null && r == null) return l;
         return root;*/
     }
 }
